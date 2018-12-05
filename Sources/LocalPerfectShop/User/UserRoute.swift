f4fd155ed84b4a090d48b3b271e7e638830aa78b
Eddie <eddie.marvin116@gmail.com>
2018-12-05 11:20:11 +0700

    Initial commit

---
+++ Sources/LocalPerfectShop/User/UserRoute.swift
@@ -0,0 +1,28 @@
+//
+//  UserRoute.swift
+//  LocalPerfectShop
+//
+//  Created by NTQ on 12/4/18.
+//
+
+import Foundation
+import PerfectHTTP
+
+struct UserRoute: PFRouteConvertible {
+    static var routes: [Route] = {
+        var routes = [Route]()
+        routes.append(Route(method: .get, uri: "/user", handler: handler))
+        return routes
+    }()
+
+    private static func handler(request: HTTPRequest, response: HTTPResponse) {
+        let u = User(username: "2", age: 24)
+        GFResponse(response, body: u)
+
+    }
+}
+
+private struct User: Codable {
+    var username: String
+    var age: Int
+}
